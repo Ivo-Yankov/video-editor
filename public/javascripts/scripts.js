0fb2496ad6e584f886704a9dd46bba3fa74d4e0c
Ivo <ivo@devlabs.bg>
2016-08-17 14:30:28 +0300

    Added zoom to the timeline

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -3,9 +3,13 @@
 		$timeline = $('#timeline');
 
 		var zoom = $timeline.attr('data-zoom') || 1,
-			ruler_markers = 60,
-			duration = 600,
-			layer_width = 200;
+			marker_density = $timeline.attr('data-markerdensity') || 60,
+			duration = $timeline.attr('data-duration') || 600,
+			layer_width = $timeline.attr('data-layerwidth') || 200;
+
+		var ruler_markers = parseInt(Math.round(zoom) * layer_width * marker_density);
+		layer_width *= zoom;
+
 
 		var duration_step = duration / ruler_markers;
 
@@ -14,7 +18,7 @@
 				$e = $(e);
 
 				// set width
-				$e.css('width', layer_width + "%");
+				$e.css('width', layer_width  + "%");
 
 				// render ruler
 				$e.append ( renderRuler() );
@@ -23,8 +27,8 @@
 			// Process the media elements
 			$('.layer-media').each(function(i,e) {
 				$e = $(e);
-				var start = parseFloat($e.attr('data-start'));
-				var media_duration = parseFloat($e.attr('data-duration'));
+				var start = Number($e.attr('data-start'));
+				var media_duration = Number($e.attr('data-duration'));
 				var new_left = ( start / duration * 100 );
 				$e.css('left', new_left + "%" );
 				$e.css('width', ( media_duration / duration * 100 ) + "%" ); 
@@ -33,13 +37,15 @@
 		}
 
 		function renderRuler() {
+			$('.timeline-ruler').remove();
+
 			var $ruler = $('<div class="timeline-ruler"></div>');
 			var $marker, time, time_string;
 
 			for (var i = 1; i < ruler_markers; i++) {
 				time = i * duration_step;
 				time = time + ""; // Convert to a string
-				$marker = $('<div class="ruler-marker">' + time.toHHMMSS() + '</div>')
+				$marker = $('<div class="ruler-marker"></div>')
 
 				
 				$marker.css('left', (time * 100 / duration) + "%");
@@ -48,6 +54,12 @@
 				if (i % 5 == 0) {
 					$marker.addClass('big');
 				}
+
+				if (i % ( 5 * marker_density ) == 0) {
+					$marker.text(time.toHHMMSS());
+				}
+
+
 
 
 				$ruler.append($marker);
@@ -63,6 +75,21 @@
 
 $(function(){
 	$.editor();
+
+	$('.zoom-button').on('click', function(){
+		$this = $(this);
+		$timeline = $('#timeline');
+		var current_zoom = Number($timeline.attr('data-zoom'));
+		var ammount = Number($this.attr('data-ammount'));
+		var new_zoom = Math.round((current_zoom + ammount) * 100) / 100 ; // Remove inaccuratcies from using float numbers
+		
+		if ( new_zoom > 0.1 && new_zoom < 10 ) {
+			$timeline.attr('data-zoom', new_zoom);
+			$.editor();
+		}
+
+
+	});
 });
 
 
