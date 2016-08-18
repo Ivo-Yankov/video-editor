0aff4e0cbd6a47636fcf4926337347808bb167d3
Ivo <ivo@devlabs.bg>
2016-08-18 18:53:29 +0300

    Added drag functionallity to the timeline media and started on syncing the backend and frontend

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,95 +1,264 @@
 (function ( $ ) {
-	$.editor = function() {
-		$timeline = $('#timeline');
+	$.editor =  {
+		timeline : "",
+		zoom : "",
+		marker_density : "",
+		duration : "",
+		layer_width : "",
+		initial_layer_width : "",
+		ruler_markers : "",
+		pressed_keys: [],
 
-		var zoom = $timeline.attr('data-zoom') || 1,
-			marker_density = $timeline.attr('data-markerdensity') || 60,
-			duration = $timeline.attr('data-duration') || 600,
-			layer_width = $timeline.attr('data-layerwidth') || 200;
+		init: function() {
 
-		var ruler_markers = parseInt(Math.round(zoom) * layer_width * marker_density);
-		layer_width *= zoom;
+			this.timeline = $('#timeline');
+			this.zoom = this.timeline.attr('data-zoom') || 1;
+			this.marker_density = this.timeline.attr('data-markerdensity') || 60;
+			this.duration = this.timeline.attr('data-duration') || 600;
+			this.layer_width = this.timeline.attr('data-layerwidth') || 200;
+			this.ruler_markers = parseInt(Math.round(this.zoom) * this.layer_width * this.marker_density);
+			this.initial_layer_width = this.layer_width;
+
+			$( ".media" ).draggable({ 
+				snap: ".layer, .layer:before",
+				snapMode: "inner",
+				snapTolerance: 10,
+				helper: function(){
+					$this = $(this);
+					var background = $this.find('img').attr('src');
+					var videoid = 'videoid';
+					var duration = '100';
 
 
-		var duration_step = duration / ruler_markers;
+					return $("<div class='layer-media' data-id='" + videoid + "' data-duration='" + duration + "' style='background-image:url(" + background + ")'></div>");
+				},
+				stop: function( event, ui ) {
+					ui.helper.remove();
+				}
+			});			
 
-		function renderLayers() {
-			$('.layer').each(function(i,e) {
+			this.render();
+
+			$('.zoom-button').on('click', function(){
+				$this = $(this);
+				var amount = Number($this.attr('data-amount'));
+
+				var current_zoom = Number($.editor.zoom);
+				var new_zoom = Math.round((current_zoom + amount) * 100) / 100 ; // Remove inaccuracies from using float numbers
+				
+				if ( new_zoom > 0.1 && new_zoom < 10 ) {
+					$.editor.zoom = new_zoom;
+					$.editor.layer_width = $.editor.initial_layer_width * new_zoom;
+					$.editor.render();
+				}
+			});	
+
+			$('#add-layer').on('click', function(){
+				$.editor.addLayer();
+			});	
+
+			$('#render').on('click', function(){
+				$.editor.renderVideo();
+			});	
+
+			this.startHotKeyListener();
+		},
+
+		render: function() {
+			this.renderLayers();
+		},
+
+		renderLayers: function( layers ) {
+			if ( !layers ) {
+				layers = $('.layer');
+			}
+
+			layers.droppable({
+				accept: ".layer-media, .media",
+				drop: function( event, ui ) {
+					$this = $(this);
+					var left_px = ui.position.left;				
+					var e;
+
+					if ( ui.helper && ui.helper !== ui.draggable ) {
+						e = ui.helper.clone();
+						$.editor.initLayerMedia(e);
+
+						var offset = $this.offset();
+						left_px -= offset.left;
+					}
+					else {
+						e = ui.draggable;
+					}
+					var left_percent = (left_px * 100) / Number($this.width());
+					var new_start = (left_percent / 100) * Number($.editor.duration);
+
+					e.css('left', left_percent + "%");
+					e.attr('data-start', new_start);
+					e.css('top', 0).detach().appendTo($this);
+				}
+			});
+			
+			layers.each(function(i,e) {
 				$e = $(e);
 
 				// set width
-				$e.css('width', layer_width  + "%");
+				$e.css('width', $.editor.layer_width  + "%");
 
 				// render ruler
-				$e.append ( renderRuler() );
+				$e.find('.timeline-ruler').remove();
+				$e.append ( $.editor.renderRuler() );
 			});
 
 			// Process the media elements
-			$('.layer-media').each(function(i,e) {
+			this.initLayerMedia();
+
+		},
+
+		initLayerMedia : function ( media ) {
+			if ( !media ) {
+				media = $('.layer-media');
+			}
+
+			media.draggable({ 
+				snap: ".layer, .layer:before",
+				snapMode: "inner",
+				snapTolerance: 10,
+				stop: function( event, ui ) {
+				},
+			    start: function(event, ui) {
+			        selectedObjs = $('.layer-media.selected');
+			    },
+			    drag: function(event, ui) {
+
+			    	/*
+					// TODO move selected media items at once
+
+			        var currentLoc = $(this).position();
+			        var orig = ui.originalPosition;
+
+			        var offsetLeft = currentLoc.left-orig.left;
+			        var offsetTop = currentLoc.top-orig.top;
+			        
+					selectedObjs.each(function(){
+				        $this = $(this);
+				        var pos = $this.position();
+
+				        var l = $this.position().clientLeft;
+				        var t = $this.position().clientTop;
+
+				        $this.css('left', l+offsetLeft);
+				        $this.css('top', t+offsetTop);
+				    })
+					*/
+			    }  
+			});
+			
+			media.each(function(i,e) {
 				$e = $(e);
 				var start = Number($e.attr('data-start'));
 				var media_duration = Number($e.attr('data-duration'));
-				var new_left = ( start / duration * 100 );
+				var new_left = ( start / $.editor.duration * 100 );
 				$e.css('left', new_left + "%" );
-				$e.css('width', ( media_duration / duration * 100 ) + "%" ); 
+				$e.css('width', ( media_duration / $.editor.duration * 100 ) + "%" ); 
 			});
 
-		}
+			media.on('click', function(e){
+				if( ! $.editor.isKeyPressed('shift') ) {
+					$('.layer-media.selected').not(this).removeClass('selected');
+				}
+				$(this).toggleClass('selected');
+			});
+		},
 
-		function renderRuler() {
-			$('.timeline-ruler').remove();
+		renderRuler: function() {
+			var duration_step = $.editor.duration / $.editor.ruler_markers;
 
 			var $ruler = $('<div class="timeline-ruler"></div>');
 			var $marker, time, time_string;
 
-			for (var i = 1; i < ruler_markers; i++) {
+			for (var i = 1; i < $.editor.ruler_markers; i++) {
 				time = i * duration_step;
 				time = time + ""; // Convert to a string
 				$marker = $('<div class="ruler-marker"></div>')
-
-				
-				$marker.css('left', (time * 100 / duration) + "%");
-
+				$marker.css('left', (time * 100 / $.editor.duration) + "%");
 
 				if (i % 5 == 0) {
 					$marker.addClass('big');
 				}
 
-				if (i % ( 5 * marker_density ) == 0) {
+				if (i % ( 5 * $.editor.marker_density ) == 0) {
 					$marker.text(time.toHHMMSS());
 				}
-
-
-
 
 				$ruler.append($marker);
 			}
 			return $ruler;
+		},
+
+		addLayer: function(){
+			var layer = $('<div class="layer"></div>');
+			this.timeline.append(layer);
+			this.renderLayers(layer);
+		},
+
+		startHotKeyListener: function( keys ){
+			$(document).on('keydown', function(e){
+				$.editor.pressed_keys[e.keyCode] = true;
+			});
+
+			$(document).on('keyup', function(e){
+				$.editor.pressed_keys[e.keyCode] = false;
+			});
+		},
+
+		isKeyPressed: function( key ){
+			var keys = {
+				'control': 17,
+				'shift': 16
+			}
+
+			return Boolean(this.pressed_keys[keys[key]]);
+		},
+
+		renderVideo: function (){
+			$form = $('#test-form');
+			var timeline_data = [];
+			$('.layer-media').each(function(i, e){
+				$e = $(e);
+				timeline_data.push({
+					'file': 'video1.mp4',
+					'start': 0,
+					'end': Number($e.attr('data-duration')),
+					'timeline_start': Number($e.attr('data-start')),
+					'timeline_layer': 0,
+					'volume': 1,
+					'top_left_x': 0,
+					'top_left_y': 0,
+					'bottom_right_x': 500,
+					'bottom_right_y': 500,
+					'has_video': true,
+					'has_audio': true
+				});
+			})
+			.promise()
+			.done( function(){
+				console.log({'clips' : timeline_data});
+				$.ajax({
+					url: "/editor",
+					data: {'clips' : timeline_data},
+					method: "post",
+					dataType: "json"
+				}).done(function( data ) {
+					console.log(data);
+				});
+			});
 		}
-
-		renderLayers();
-
-		return this;
 	}
 }( jQuery ));
 
 $(function(){
-	$.editor();
-
-	$('.zoom-button').on('click', function(){
-		$this = $(this);
-		$timeline = $('#timeline');
-		var current_zoom = Number($timeline.attr('data-zoom'));
-		var ammount = Number($this.attr('data-ammount'));
-		var new_zoom = Math.round((current_zoom + ammount) * 100) / 100 ; // Remove inaccuratcies from using float numbers
-		
-		if ( new_zoom > 0.1 && new_zoom < 10 ) {
-			$timeline.attr('data-zoom', new_zoom);
-			$.editor();
-		}
-
-
-	});
+	$.editor.init();
 });
 
 
